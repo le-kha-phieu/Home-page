783f9f423add6e61d9b0039c4bee3ba2594cf39f
le-kha-phieu <140295709+le-kha-phieu@users.noreply.github.com>
2023-08-06 10:35:54 +0700

    fix bug new (#1)
    * Design homepage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,11 @@
 window.addEventListener('scroll', () => {    
     var headerFix = document.querySelector('.header-under');
-    if (window.scrollY > 200) {
+    if (window.scrollY > 180) {
         headerFix.classList.add('header-under-fix');
     } else {
         headerFix.classList.remove('header-under-fix');
-    }
+    }   
 })
 
 
-
-// var header = document.getElementById('header');
-// window.onscroll = function () { 
-//     "use strict";
-//     if (document.body.scrollTop >= 50 ) {
-//         header.classList.add("header-under-fix");
-//         // header.classList.remove("header-under-fix");
-//     } 
-//     else {
-//         // header.classList.add("header-under-fix");
-//         header.classList.remove("header-under-fix");
-//     }
-// };
-
-
-
+//sửa rất nhiều file
